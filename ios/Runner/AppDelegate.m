b1c68e09020b4c8395956859d02e6b348dc215cc
jsf <jsfsarkis@gmail.com>
2021-03-16 14:29:29 +0200

    implement google maps

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,10 +1,12 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [GMSServices provideAPIKey:@"AIzaSyB1wn7gcNBpxeBM9MY3_EMXoP-woMMIFnU"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,8 +1,40 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomeTab extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeTab extends StatefulWidget {
+  @override
+  _HomeTabState createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  GoogleMapController mapController;
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Hello Home Tab'));
+    return SafeArea(
+      child: Stack(
+        children: [
+          GoogleMap(
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+            zoomControlsEnabled: false,
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+              mapController = controller;
+            },
+          )
+        ],
+      ),
+    );
   }
 }
